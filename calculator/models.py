74df7b5d11198de1578c1f494c91188f04657910
carlos <cdaza933@hotmail.com>
2019-10-01 22:01:16 -0500

    paso 3- modificar codigo para futuros cambios

--- calculator/models.py
+++ calculator/models.py
@@ -3,5 +3,9 @@ from django.db import models
 # Create your models here.
 
 
-def calculadora(param:str):
-    return 0 if param == "" else len(param.split(","))
+def calculadora(param: str):
+    if param == "":
+        return 0
+    else:
+        list_of_numbers = [float(str_number) for str_number in param.split(",")]
+        return len(list_of_numbers)
