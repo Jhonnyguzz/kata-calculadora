ea02bbfc324190ecd7e875a904336dd680d84ad3
carlos <cdaza933@hotmail.com>
2019-10-01 21:14:48 -0500

    paso1-rojo create test for each case in iteration 1

--- calculator/tests.py
+++ calculator/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
 
 # Create your tests here.
+from calculator.models import calculadora
+
+
+class CalculadoraTest(TestCase):
+    def iteracion_1c1(self):
+        self.assertEquals(calculadora(""), 0)
+
+    def iteracion_1c2(self):
+        self.assertEquals(calculadora("1"), 1)
+
+    def iteracion_1c3(self):
+        self.assertEquals(calculadora("1,2"), 2)
+
+    def iteracion_1c4(self):
+        self.assertEquals(calculadora("1,2,3,4,5,6,6,8"), 8)
